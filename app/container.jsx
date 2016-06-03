8d71c1c465ff33316c6e0c64043cdb92db3aac8e
Filip Stenbeck <filip.stenbeck@svt.se>
2016-06-03 09:41:47 +0200

    new container layout

--- app/container.jsx
+++ app/container.jsx
@@ -4,16 +4,14 @@ import Message from './message.jsx';
 export default class Container extends React.Component {
 
   render() {
-		const hello = this.props.hello;
-		const world = this.props.world;
-
-
+    const hello = this.props.hello;
+    const world = this.props.world;
     return (
-			<div>
-				<Message message={hello}/>
+      <div>
+        <Message message={hello}/>
         &nbsp;
         <Message message={world}/>
-		</div>
-		)
+      </div>
+    )
   }
 }
