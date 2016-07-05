6299a68720fc73fa26ae84427ce4b37dae6c6bfb
Filip Stenbeck <filip.stenbeck@svt.se>
2016-07-05 12:13:27 +0200

    added css-modules

--- app/components/message/messages.jsx
+++ app/components/message/messages.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import Message from './message.jsx';
-
+import style from "./messages.css";
 
 export default class Messages extends React.Component {
 
   render() {
-
     return (
       <div>
         <Message message={this.props.hello}/>
         &nbsp;
         <Message message={this.props.world}/>
         <p>
-          <button onClick={this.props.setMessage}>Translate</button>
+          <button className={style.btn} onClick={this.props.setMessage}>Translate</button>
         </p>
       </div>
     )
